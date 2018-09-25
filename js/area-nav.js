cdd0887e7f164d774d93a1c32e5078bcb038a598
Klar-t <qqxxhhzz@126.com>
2018-09-25 11:27:16 +0800

    添加注释

---
+++ js/area-nav.js
@@ -0,0 +1,12 @@
+
+$(function(){
+	
+	$('.nav-aside .category').on('mouseenter',function(){
+		$(this).addClass('hover');
+		
+	})
+	$('.nav-aside .category').on('mouseleave',function(){
+		
+		$(this).removeClass('hover');
+	})
+})
